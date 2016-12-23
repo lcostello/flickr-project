4bbc511b27c8f8e105f1317c177b99f13413c385
Tech-A41 <Tech-A41@Tech-A41s-MacBook-Air.local>
2016-12-23 10:56:45 +0000

    moved procfile to correct location

--- test/test-function.js
+++ test/test-function.js
@@ -1,15 +1,20 @@
-const assert = require ("assert");
 const chai = require('chai');
 const should = chai.should();
 const expect = require('chai').expect;
-const insert = require('chai').assert;
-const Functions = require("../functions");
-// const func = new Functions();
+const assert = require('chai').assert;
+const FlickrPhoto = require("../models/flickrData");
 
 
-describe('FlickrPhoto', function() {
-    it('should exist', function() {
-        var FlickrPhoto = require('./models/flickrData');
-        expect(FlickrPhoto).to.not.be.undefined;
+describe('FlickrPhoto', () => {
+    it('should exist', (done) => {
+        let testObject = {id: "31388261960", 
+                         title: "Banana bread Recipe", 
+                         latitude: "54.925464", 
+                         longitude: "9.808859", 
+                         url_l: "https://farm1.staticflickr.com/750/31388261960_07e524b611_b.jpg"
+        };
+        let modelObject = new FlickrPhoto(testObject);
+        assert.isObject(modelObject,"it is an object");
+        done();
     });
 });
